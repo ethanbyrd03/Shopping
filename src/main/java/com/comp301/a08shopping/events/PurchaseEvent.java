1177beb5751065b8da5ee5e0a822c503246135fb
Ethan Byrd <ethbyr@ad.unc.edu>
2023-11-15 14:12:24 -0500

    Novice

---
+++ src/main/java/com/comp301/a08shopping/events/PurchaseEvent.java
@@ -0,0 +1,24 @@
+package com.comp301.a08shopping.events;
+
+import com.comp301.a08shopping.Product;
+import com.comp301.a08shopping.Store;
+
+public class PurchaseEvent implements StoreEvent {
+  private final Product _product;
+  private final Store _store;
+
+  public PurchaseEvent(Product product, Store store) {
+    this._product = product;
+    this._store = store;
+  }
+
+  @Override
+  public Product getProduct() {
+    return this._product;
+  }
+
+  @Override
+  public Store getStore() {
+    return this._store;
+  }
+}
