737c12401545d5fb033edfedbda0d246bd9707cd
Ethan Byrd <ethbyr@ad.unc.edu>
2023-11-15 19:15:06 -0500

    all

--- src/main/java/com/comp301/a08shopping/CustomerImpl.java
+++ src/main/java/com/comp301/a08shopping/CustomerImpl.java
@@ -36,7 +36,7 @@ public class CustomerImpl implements Customer {
       throw new IllegalArgumentException();
     }
     if (product.getBasePrice() > this._budget) {
-      throw new IllegalArgumentException();
+      throw new ProductNotFoundException();
     }
     ReceiptItem receipt = store.purchaseProduct(product);
     this._history.add(receipt);
